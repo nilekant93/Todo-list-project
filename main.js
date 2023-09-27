cb50e78536d6a5c2f49bd521ee848102480f1a13
nilekant93 <nikolas.hirvonen@gmail.com>
2023-09-27 20:05:40 +0300

    Javasript working
    Texts from all fields now moves to the "Taskbox" on the left.

--- main.js
+++ main.js
@@ -1,8 +1,51 @@
-const sendButton = document.getElementById("sendButton");
-const titleInput = document.querySelector(".setTitle");
+document.addEventListener('DOMContentLoaded', () => {
+    const sendButton = document.getElementById("sendButton");
+    const titleInput = document.querySelector(".setTitle");
+    const dueInput = document.querySelector("input[name='due']");
+    const descriptionInput = document.querySelector(".setDesc");
 
+    const outputContainer = document.getElementById("tavaralaatikko");
+    const outputTitle = document.getElementById("outputTitle");
+    const outputDue = document.getElementById("outputDate");
+    const outputDescription = document.getElementById("outputdesc");
 
+    const addButton = document.getElementById("addButton");
 
-sendButton.addEventListener("click", function () {
-    titleInput.value = "Hello World";
+    sendButton.addEventListener("click", function () {
+
+        const title = titleInput.value;
+        const due = dueInput.value;
+        const description = descriptionInput.value;
+
+        if (title.trim() === "") {
+            alert("Title field cannot be empty!");
+            return; 
+        }
+
+        outputTitle.textContent = title;
+        outputDue.textContent =  due;
+        outputDescription.textContent = description;
+
+        titleInput.value = "";
+        dueInput.value = "";
+        descriptionInput.value = "";
+
+    
+
+    });
+
+    var state = false;
+    addButton.addEventListener("click", function(){
+        if (state) {
+            state = false;
+
+            addButton.innerHTML = "+";
+            document.getElementById("sl1").style.display = "none";
+        }else{
+            state = true;
+
+            addButton.innerHTML = "-";
+        document.getElementById("sl1").style.display = "block";
+        }
+    })
 });
